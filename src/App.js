9ed0c4255898f3f94933b4ac84b62d82a42b59a5
Veri Ferdiansyah <veri_ferdi@outlook.com>
2022-02-15 22:48:00 +0700

    feat: add upload functionality

--- src/App.js
+++ src/App.js
@@ -1,25 +1,71 @@
-import logo from './logo.svg';
-import './App.css';
+import axios from 'axios';
+import React, { useState } from 'react';
+import { toast } from 'react-toastify';
 
-function App() {
+const { REACT_APP_UPLOAD_API } = process.env;
+
+const App = () => {
+  const [file, setFile] = useState(null);
+  const [isDisabled, setIsDisabled] = useState(false);
+
+  const handleFileChange = (e) => {
+    const files = e.target.files;
+    setFile(files[0]);
+  }
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setIsDisabled(true);
+
+    const formData = new FormData();
+    formData.append('file', file);
+
+    const response = await toast.promise(axios.post(`${REACT_APP_UPLOAD_API}/uploads/v1`, formData, {
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+        'Content-Type': 'multipart/form-data'
+      }
+    }), {
+      pending: 'Uploading file...',
+      success: 'File uploaded!',
+      error: 'Error while uploading file'
+    });
+
+    if (response) {
+      setIsDisabled(false);
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="flex justify-center">
+      <form onSubmit={handleSubmit}>
+        <div className="form-group mb-3 w-96">
+          <label htmlFor="formFile" className="form-label inline-block mb-2 text-gray-700">
+            Upload file
+          </label>
+          <input
+            className="form-control block w-full px-3 py-1.5 text-base font-normal
+                      text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300
+                      rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white
+                      focus:border-blue-600 focus:outline-none"
+            type="file"
+            id="formFile"
+            accept=".zip"
+            onChange={handleFileChange} />
+        </div>
+        <button
+          type="submit"
+          className="px-6 py-2.5 bg-blue-600 text-white font-medium text-xs
+                    leading-tight uppercase rounded shadow-md hover:bg-blue-700
+                    hover:shadow-lg focus:bg-blue-700 focus:shadow-lg
+                    focus:outline-none focus:ring-0 active:bg-blue-800
+                    active:shadow-lg transition duration-150 ease-in-out"
+          disabled={isDisabled}>
+          Upload
+        </button>
+      </form>
     </div>
-  );
-}
+  )
+};
 
 export default App;
